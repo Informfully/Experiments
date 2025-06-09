80ed21f1977299550bdcfc312f7a546bc16ca4b2
Lucien Heitz <lucienheitz@msn.com>
2025-06-09 20:24:51 +0200

    Update data_cleaning_ebnerd.py

--- experiments/tutorial/article_enrichment_scripts/data_cleaning_ebnerd.py
+++ experiments/tutorial/article_enrichment_scripts/data_cleaning_ebnerd.py
@@ -7,11 +7,9 @@ import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, current_dir)
 
-
 input_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'ebnerd_input')
 
-
-# load EB-NERD dataset
+# Load EB-NERD dataset
 articles_path = os.path.join(input_folder, 'articles.parquet')
 train_behaviors_path = os.path.join(input_folder, 'behaviors-train.parquet')
 train_history_path = os.path.join(input_folder, 'history-train.parquet')
@@ -49,13 +47,13 @@ def combine_title_and_subtitle(row):
     elif title_clean:
         return title_clean
     else:
-        # subtitle only, or empty if both are empty
+        # Subtitle only, or empty if both are empty
         return subtitle_clean
 
 articles_df["new_title"] = articles_df.apply(combine_title_and_subtitle, axis=1)
 
 
-# check if a value is missing
+# Check if a value is missing
 def is_missing(value):
     return (
         pd.isnull(value)
@@ -63,7 +61,7 @@ def is_missing(value):
     )
 
 
-# check if a row is incomplete
+# Check if a row is incomplete
 def is_incomplete(row):
     return (
         is_missing(row["published_time"])
@@ -81,12 +79,12 @@ print("Incomplete article IDs:")
 print(incomplete_ids)
 print(f"Len incomplete articles:{len(incomplete_ids)}")
 
-# prepare for the cleaned article data
-# remove rows where 'article_id' is in incomplete_ids
+# Prepare for the cleaned article data
+# Remove rows where 'article_id' is in incomplete_ids
 articles_df = articles_df[~articles_df['article_id'].isin(incomplete_ids)]
 print(f"Remaining rows: {len(articles_df)}")
 
-# create the new DataFrame with correct mappings
+# Create the new DataFrame with correct mappings
 articles_df = articles_df.drop(columns=['title', 'category'], errors='ignore')
 new_df = articles_df.rename(columns={
     'article_id': 'id',
@@ -99,21 +97,17 @@ new_df = articles_df.rename(columns={
 print(new_df.head())
 print(f"len cleaned EB_Nerd articles: {len(new_df)}")
 
-
-
-
 output_folder = './ebnerd_results'
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-
-# save the cleaned articles to csv file. change the output path.
+# Save the cleaned articles to csv file. change the output path.
 output_cleaned_article_path = os.path.join(output_folder, "cleaned_articles.csv")
 new_df.to_csv(output_cleaned_article_path, index=False)
 
 incomplete_ids_path = os.path.join(output_folder, "incomplete_article_ids.txt")
-# save the removed article ids to text. change the output path.
+# Save the removed article ids to text. change the output path.
 with open(incomplete_ids_path, 'w') as f:
     for article_id in incomplete_ids:
         f.write(f"{article_id}\n")
