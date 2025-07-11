f58aa0dec81e867ebad41de0b171df2adb2b0f4a
Lucien Heitz <lucienheitz@msn.com>
2025-07-11 04:34:56 +0200

    Update augmentation.py

--- experiments/recsys_paper_2023/scraperpackage/pipeline/augmentation.py
+++ experiments/recsys_paper_2023/scraperpackage/pipeline/augmentation.py
@@ -12,26 +12,23 @@ def augment(staging_collection, categories_collection, parties_collection, polit
     # Calculate the word count of the body content and update the document
     update_body_word_counts(staging_collection)
 
-    # Wherever lead is empty, fill it up with first two sentences from the body content
+    # Wherever lead is empty, fill it up with the first two sentences from the body content
     fill_lead_from_article_body(staging_collection)
 
     # Load the party names and politicians, followed by calculating their references in the articles
     findPoliticalReferences(staging_collection, parties_collection, politicians_collection)   
 
-    # If there is any political reference in the article meaning, any party name or political person is mentioned in the 
-    # article then the category of that article would be changed to Panorama only if the cateogory of that article is not listed
+    # If there is any political reference in the article's meaning, any party name or political person is mentioned in the 
+    # article, then the category of that article would be changed to Panorama only if the category of that article is not listed
     # categories collection
     update_primay_category(staging_collection, categories_collection)
 
-    # # take the image url from teh categories collection and update the url in staging against the article based on its category
+    # Take the image URL from the categories collection and update the URL in staging against the article based on its category
     update_image_urls(staging_collection, categories_collection)
     
-
-    # # Validate the article based on certain condition
-    # # if it passes through all the conditions, only then it will be pushed to Articles collection 
-    # # in the next step along with duplicate detection
+    # If it passes through all the conditions, only then will it be pushed to the articles collection 
+    # in the next step, along with duplicate detection
     validateArticle(staging_collection)
-
 
     valid_articles_count = staging_collection.count_documents({'is_valid': True })
     return valid_articles_count
@@ -54,7 +51,7 @@ def fill_lead_from_article_body(staging_collection):
 
     start_time = datetime.now() 
 
-    # Only pull the articles where lead is empty and the body length is  more than and equal to 150     
+    # Only pull the articles where the lead is empty and the body length is  more than or equal to 150     
     staging_cursor = staging_collection.find({ '$and': [ { 'lead': '' }, 
                                                          { 'body_word_count': { '$gte': 150 } } ] })
     for staging_article in staging_cursor:
@@ -82,8 +79,8 @@ def findPoliticalReferences(staging_collection, parties_collection, politicians_
 
     # Get all the distinct party categories from the collection
     # party_categories = sorted(parties_collection.distinct('party_category'))
-    # ideally we should get the distinct categories from the collection but we sort them
-    # in a specific order we are maintainig the list manually
+    # Ideally, we should get the distinct categories from the collection, but we sort them
+    # in a specific order, we are maintaining the list manually
     party_categories = ['SPD', 'CDU/CSU', 'DIE GRÜNE', 'AfD', 'DIE LINKE', 'FDP', 'Other']
 
     staging_cursor = staging_collection.find({ '$and': [ { 'body_word_count': { '$gte': 150 } } ] })
@@ -97,7 +94,7 @@ def findPoliticalReferences(staging_collection, parties_collection, politicians_
                 
         for party_category in party_categories:
 
-            # set all the counters to zero at the category level
+            # Set all the counters to zero at the category level
             title_party_ref_count = lead_party_ref_count = body_party_ref_count = 0
             title_politician_ref_count = lead_politician_ref_count = body_politician_ref_count = 0
             political_references_count_party_wise = 0
@@ -105,7 +102,7 @@ def findPoliticalReferences(staging_collection, parties_collection, politicians_
             parties_cursor = parties_collection.find({ 'party_category': {'$regex': party_category} })
             for party in parties_cursor:                
 
-                # these three variables hold the counts returned by the function/method
+                # These three variables hold the counts returned by the function/method
                 _title_party_ref_count, _lead_party_ref_count, _body_party_ref_count, _party_names_found = search_party_in_article(staging_article, party)
 
                 title_party_ref_count += _title_party_ref_count
@@ -115,13 +112,13 @@ def findPoliticalReferences(staging_collection, parties_collection, politicians_
                 if (_party_names_found != ''):
                     party_names_found += _party_names_found
 
-                    # If party is mentioned, only then its politicians will be checked in the articles. Otherwise No.
+                    # If a party is mentioned, only then will its politicians be checked in the articles. Otherwise No.
 
                     # politicians_cursor = politicians_collection.find({ 'party_name': {'$regex': party['party_name'], '$options': 'i'} })
                     politicians_cursor = politicians_collection.find({ 'party_name': party['party_name'] })
                     for politician in politicians_cursor: 
 
-                        # these three variables hold the counts returned by the function/method
+                        # These three variables hold the counts returned by the function/method
                         _title_politician_ref_count, _lead_politician_ref_count, _body_politician_ref_count = search_politician_in_article(staging_article, politician)
 
                         if (_title_politician_ref_count + _lead_politician_ref_count + _body_politician_ref_count > 0):
@@ -170,7 +167,7 @@ def search_party_in_article(article, party):
     matches_found_body = matches_found_title = matches_found_lead = 0
     party_names_found = ''
 
-    # party name is not part of Aliases, so add it to the aliases list so that can be matched as well
+    # The party name is not part of aliases, so add it to the aliases list so that it can be matched as well
     party['aliases'].append(party['party_name'])
 
     for party_name_alias in party['aliases']:
@@ -199,7 +196,7 @@ def search_politician_in_article(article, politician):
     regex_lastname_without_firstname = r"(?<!"+ politician['first_name'] + "\s)" + politician['last_name'] 
     regex_firstname_without_lastname = r"" + politician['first_name'] + "(?!\s+" + politician['last_name'] + ")"
     
-    #if full name is equal to (first + last) name then apply the regex_lastname_with_firstname
+    # If the full name is equal to (first + last) name then apply the regex_lastname_with_firstname
     # otherwise match the full name
     if politician['full_name'] == politician['fl_name']:
         full_name_match_found_in_body = len(list(re.finditer(regex_lastname_with_firstname, article['body'], re.MULTILINE | re.IGNORECASE)))      
@@ -236,7 +233,7 @@ def update_primay_category(staging_collection, categories_collection):
     for staging_article in staging_cursor:
 
         if staging_article['primary_category'] == "":
-            primary_category = 0 # means this does not exist in categories collection
+            primary_category = 0 # means this does not exist in the categories collection
         else:
             primary_category = categories_collection.count_documents({ "$and": [
                     {'outlet': staging_article['outlet'].replace('WISO-','')},
@@ -267,13 +264,13 @@ def update_image_urls(staging_collection, categories_collection):
                 {'primary_category': {'$regex': staging_article['primary_category'], '$options': 'i'} } 
                 ] })
         
-        # only update the image url for the articles whose categories are present in "categories" collection. 
+        # Only update the image URL for the articles whose categories are present in the "categories" collection. 
         if category is not None:        
             updated_values = { "$set": { "image_url": category['image_url'] } }
             update_query = { "_id": staging_article['_id'] }
             staging_collection.update_one(update_query, updated_values)
 
-    # fill the image_url with empty string for all the articles whose category is not listed in the categories collection.
+    # Fill the image_url with an empty string for all the articles whose category is not listed in the categories collection.
     staging_collection.update_many(        
         { "image_url": { '$exists': False } }, 
         { "$set": { "image_url": '' } }
@@ -300,20 +297,20 @@ def validateArticle(staging_collection):
 
         reason = ''
 
-        #body length is less than 150 words
+        # Exclude the article if the body length is less than 150 words
         if (int(staging_article['body_word_count']) < 150):
             reason += 'Body text is less than 150 words; '
 
-        #Both title and Lead are empty;
+        # Exclude article if both title and Lead are empty;
         elif (staging_article['title'].strip() == '' and staging_article['lead'].strip() == ''):
             reason += 'Both title and Lead are empty; '  
 
-        # if article belongs to a category which is not in allowed list (categories collection)
+        # If the article belongs to a category that is not in the allowed list (categories collection)
         # then the image_url would be empty. So we can safely check image_url and invalidate that particular article
         elif (staging_article['image_url'] is None or staging_article['image_url'] == ''):
             reason += 'Category is not in allowed list; '
 
-        # this should be checked in the end, as the time to calculate the title length is high.
+        # This should be checked in the end, as the time to calculate the title length is high.
         # Title is less than 2 words
         if (calculateWordCountLength(staging_article['title'].strip()) < 2):
             reason = 'Title is less than 2 words; '            
